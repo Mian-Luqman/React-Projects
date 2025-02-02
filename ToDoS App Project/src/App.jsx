799173c4acc59126a152db6978415498a2dcca19
Muhammad Luqman <mianluqman09876@gmail.com>
2025-02-03 02:16:41 +0500

    Add files via upload

---
+++ ToDoS App Project/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { TodoProvider } from "./contexts";
+import "./App.css"; // Use Vanilla CSS
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem(""));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="app-container">
+        <div className="app-wrapper">
+          <h1 className="app-title">Manage Your Todos</h1>
+          <div className="app-form">
+            <TodoForm />
+          </div>
+          <div className="app-todo-list">
+            {todos.map((todo) => (
+              <div key={todo.id} className="app-todo-item">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App;
